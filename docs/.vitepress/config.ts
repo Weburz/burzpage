95d0efb8d4ba6b3c82b4b1f1eba6ac4fc8e8c133
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-05-07 09:17:36 +0530

    chore: initialise the docs folder for the project

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  title: "PixiePress",
+  description:
+    "A tiny yet versatile and super-fast CMS for online business owners!",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
+    ],
+
+    sidebar: [
+      {
+        text: "Examples",
+        items: [
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
