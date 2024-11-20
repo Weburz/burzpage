258fd027a93fffb1531b7c39b0f4e151cd4e01ff
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-11-20 16:52:42 +0530

    chore: add Swagger docs support

--- server/main.go
+++ server/main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/swaggo/fiber-swagger"
+
+	_ "github.com/Weburz/burzpress/server/docs"
+)
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
 func main() {
-	println("Hello World!")
+	app := fiber.New(fiber.Config{
+		AppName:       "BurzPress API",
+		StrictRouting: true,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Get("/swagger/*", fiberSwagger.WrapHandler)
+
+	if err := app.Listen(":8080"); err != nil {
+		log.Fatal("Failed to load the server: %w", err)
+	}
 }
