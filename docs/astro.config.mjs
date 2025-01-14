0281f882de6173b5e0fb4169693c20afa6d3089e
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-01-14 14:10:40 +0530

    chore: update and configure the Astro docs to be apt

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,28 +1,42 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://weburz.github.io/burzpress",
+  base: "burzpress",
+  integrations: [
+    starlight({
+      title: "BurzPress",
+      description:
+        "A fast & lightweight CMS for your blogging needs at any scale!",
+      editLink: {
+        baseUrl: "https://github.com/Weburz/burzpress/edit/main/docs",
+      },
+      social: {
+        github: "https://github.com/Weburz/burzpress",
+        discord: "https://discord.gg/QeYqwyxBhR",
+        email: "mailto:contact@weburz.com",
+        facebook: "https://www.facebook.com/Weburz",
+        instagram: "https://www.instagram.com/weburzit",
+        linkedin: "https://www.linkedin.com/company/weburz",
+        youtube: "https://www.youtube.com/@Weburz",
+        twitter: "https://x.com/weburz",
+      },
+      lastUpdated: true,
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
